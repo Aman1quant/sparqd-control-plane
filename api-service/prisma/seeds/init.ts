6e956f932ccef13f8179801c67e3fc72aa0eda4b
Taufiq Ibrahim <taufiq.ibrahim@gmail.com>
2025-07-13 11:30:16 +0700

    update initial tables and add seeds

---
+++ api-service/prisma/seeds/init.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '../app/generated/prisma/client';
+const prisma = new PrismaClient();
+
+async function seedInitialRoles() {
+  const roles = [
+    'Admin',
+    'Member',
+    'WorkspaceAdmin',
+  ];
+  for (const r of roles) {
+    await prisma.role.upsert({
+      where: { name: r },
+      update: {},
+      create: { name: r },
+    });
+  }
+}
+
+async function main() {
+  await seedInitialRoles();
+}
+
+main().then(() => prisma.$disconnect());

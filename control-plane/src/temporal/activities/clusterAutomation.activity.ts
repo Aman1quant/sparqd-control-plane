66757c8e7edaacc64d61751416ce694b4bc260d0
Taufiq Ibrahim <taufiq.ibrahim@gmail.com>
2025-08-01 07:04:34 +0700

    clusterAutomation: implement tofu init & plan

--- control-plane/src/temporal/activities/clusterAutomation.activity.ts
+++ control-plane/src/temporal/activities/clusterAutomation.activity.ts
@@ -1,3 +1,69 @@
-export async function sendEmail(userId: string): Promise<void> {
-  console.log(`Sending email to user ${userId}`);
+import { exec } from 'child_process';
+import pino from 'pino';
+import { promisify } from 'util';
+import { Context } from '@temporalio/activity';
+
+
+const execAsync = promisify(exec);
+const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      singleLine: false,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname',
+    },
+  },
+});
+
+/**
+ * Run an OpenTofu CLI command like `tofu plan` or `tofu apply`.
+ * @param command One of 'plan' or 'apply'
+ * @param workingDir Directory where terraform files exist
+ * @returns stdout string or throws on error
+ */
+async function runTofu(
+  command: string,
+  workingDir: string
+): Promise<string> {
+
+  logger.info({ command: command, cwd: workingDir }, `Running OpenTofu command`);
+
+  const ctx = Context.current();
+  const heartbeatInterval = setInterval(() => {
+    ctx.heartbeat(`Running ${command}`);
+  }, 10000); // heartbeat every 10s
+
+
+  try {
+    const { stdout, stderr } = await execAsync(command, {
+      cwd: workingDir,
+      env: process.env,
+    });
+
+    if (stderr?.trim()) {
+      logger.warn({ stderr: stderr.trim(), command }, 'OpenTofu stderr');
+    }
+
+    if (stdout?.trim()) {
+      logger.info({ stdout: stdout.trim(), command }, 'OpenTofu stdout');
+    }
+
+    return stdout;
+  } catch (error: any) {
+    const errMsg = error.stderr || error.message;
+    logger.error({ error: errMsg, command }, 'OpenTofu command failed');
+    throw new Error(`tofu ${command} failed: ${errMsg}`);
+  } finally {
+    clearInterval(heartbeatInterval);
+  }
+}
+
+export async function tofuInit(workingDir: string) {
+  await runTofu('tofu init ', workingDir)
+}
+
+export async function tofuPlan(workingDir: string) {
+  await runTofu('tofu plan -no-color', workingDir)
 }

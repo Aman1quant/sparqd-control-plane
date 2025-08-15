ab54ffca07080a44df1f55b4d1115bad8b0f83af
Taufiq Ibrahim <taufiq.ibrahim@gmail.com>
2025-08-15 14:27:17 +0700

    wip: remove build from git and update .gitignore

--- control-plane/build/src/domains/session/session.validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_validator_1 = require("express-validator");
-const sessionValidator = {
-    switchSession: [(0, express_validator_1.body)('accountUid').isUUID(), (0, express_validator_1.body)('workspaceUid').isUUID()],
-};
-exports.default = sessionValidator;
--- control-plane/build/tests/__mocks__/snappy.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    compress: jest.fn().mockReturnValue(Buffer.from('compressed')),
-    uncompress: jest.fn().mockReturnValue(Buffer.from('uncompressed')),
-};
